d54f575a35508aa938a23cee88e6e48d22f1c144
mukul <sonimukul456@gmail.com>
2022-08-07 00:07:39 +0530

    form changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -33,29 +33,30 @@ function Modal({setViewModal, viewModal}) {
 
   return (
     <div onClick={()=>setViewModal(!viewModal)} className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover" id="modal-id">
-    <div className="absolute h-full w-full bg-black opacity-80 inset-0 -z-10"></div>
-    <div className='relative w-full h-screen justify-center flex items-start p-10'>
- <div className="relative w-full flex flex-col max-w-[400px] shrink py-2 px-5  rounded-xl shadow-lg  bg-white ">
+    <div className="absolute h-full w-full bg-gradient-to-br from-purple-600 to-blue-500 inset-0 -z-10"></div>
+    <div className='relative w-full h-screen items-center justify-center flex p-3'>
+ <div className="relative w-full flex flex-col max-w-[400px] shrink py-2 px-5  rounded-xl shadow-lg  bg-gray-900 ">
    {/* <!--content--> */}
-   <div className=' flex justify-center'><img className='w-80' src={modal1} alt="" /></div>
+   <div className='text-left  text-lg  px-3 pt-10 pb-4 text-gray-300'>Please enter your details to get a call back from <span className='text-xl '> Career Mentor</span> </div>
+   {/* <div className=' flex justify-center'><img className='w-80' src={modal1} alt="" /></div> */}
    {/* <div className='text-2xl text-left text-gray-800 p-3'>Please enter your details to get a call back from a Career Mentor</div> */}
-   <div onClick={()=>setViewModal(!viewModal)} className='absolute right-4 top-3'> <XIcon className='w-7 p-1 hover:text-[#FF0049]'></XIcon> </div>
+   <div onClick={()=>setViewModal(!viewModal)} className='absolute right-4 top-3'> <XIcon className='w-7 p-1 text-white hover:text-blue-500'></XIcon> </div>
    <div className="">
    <form onSubmit={onSubmit} action="" className=''>
      {/* <!--body--> */}
-     <div className='p-3 space-y-5'>
+     <div className='p-3 py-6 space-y-5'>
                 <div className='space-y-7'>
                 <div className='w-full text-left'>
-                    <p className='text-sm 2xl:text-base'>Your Name</p>
-                    <input onChange={(e)=>{setName(e.target.value)}} className='text-[0.95rem] 2xl:text-lg outline-none border-b-2 w-full' placeholder='Your Name' type="text" name="name" id="name" />
+                    <p className='text-sm 2xl:text-base text-gray-100'>Your Name</p>
+                    <input onChange={(e)=>{setName(e.target.value)}} className='  text-[0.95rem] 2xl:text-lg outline-none border-gray-800 bg-transparent border-b-2 w-full' placeholder='Your Name' type="text" name="name" id="name" />
                 </div>
                 <div className='w-full text-left'>
-                    <p className='text-sm 2xl:text-base'>Email</p>
-                    <input onChange={(e)=>{setEmail(e.target.value)}} className='text-[0.95rem] 2xl:text-lg outline-none border-b-2 w-full' placeholder='Enter Your Email Address' type="email" name="email" id="email" />
+                    <p className='text-sm 2xl:text-base text-gray-100'>Email</p>
+                    <input onChange={(e)=>{setEmail(e.target.value)}} className='  text-[0.95rem] 2xl:text-lg outline-none border-gray-800 bg-transparent border-b-2 w-full' placeholder='Enter Your Email Address' type="email" name="email" id="email" />
                 </div>
                 <div className='w-full text-left'>
-                    <p className='text-sm 2xl:text-base'>Mobile No</p>
-                    <input onChange={(e)=>{setMobileNo(e.target.value)}} className='text-[0.95rem] 2xl:text-lg outline-none border-b-2 w-full' placeholder='Enter Your Mobile No' type="number" name="number" id="number" />
+                    <p className='text-sm 2xl:text-base text-gray-100'>Mobile No</p>
+                    <input onChange={(e)=>{setMobileNo(e.target.value)}} className=' text-[0.95rem] 2xl:text-lg outline-none border-gray-800 bg-transparent border-b-2 w-full' placeholder='Enter Your Mobile No' type="number" name="number" id="number" />
                 </div>
 
                 </div>
@@ -63,7 +64,7 @@ function Modal({setViewModal, viewModal}) {
             </div>
      {/* <!--footer--> */}
      <div className="p-3  mt-2 text-center space-x-4 md:block">
-         <button type='submit' className="mb-2 2xl:text-lg md:mb-0 bg-[#FF0049] border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-[#F10045]">Submit</button>
+         <button type='submit' className="mb-2 2xl:text-lg md:mb-0 bg-gradient-to-br from-purple-600 to-blue-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white w-full hover:shadow-lg ">Submit</button>
      </div>
      </form>
    </div>
